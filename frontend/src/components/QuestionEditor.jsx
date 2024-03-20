8ea19e1a3ae61a4265010f2dae721d4c231c2be9
darienvr <118293353+darienvr@users.noreply.github.com>
2024-03-19 22:39:39 -0500

    add more type of question

--- frontend/src/components/QuestionEditor.jsx
+++ frontend/src/components/QuestionEditor.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useStateContext } from "../contexts/ContextProvider";
 import { PlusIcon, TrashIcon } from "@heroicons/react/24/outline";
+import { v4 as uuidv4} from 'uuid';
 
 const QuestionEditor = ({
     index=0,
@@ -19,6 +20,41 @@ const QuestionEditor = ({
 
     function upperCaseFirst(str){
         return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+
+    function shouldHaveOptions(type = null) {
+        type = type || model.type
+        return ['select', 'radio', 'checkbox'].includes(model.type)
+    }
+
+    function onTypeChange(e) {
+        const newModel = {
+            ...model,
+            type: e.target.value
+        }
+        if(!shouldHaveOptions(model.type) && shouldHaveOptions(e.target.value)){
+            if(!model.data.options){
+                newModel.data = {
+                    options: [
+                        {uuid: uuidv4(), text: ''}
+                    ]
+                };
+            }
+        }
+        setModel(newModel)
+    } 
+
+    function addOption(){
+        model.data.options.push({
+            uuid: uuidv4(),
+            text: '',
+        })
+        setModel({...model})
+    }
+
+    function deleteOption(){
+        model.data.options = model.data.options.filter(item=>item.uuid != op.uuid)
+        setModel({...model})
     }
 
   return (
@@ -102,8 +138,7 @@ const QuestionEditor = ({
               id="questionType"
               name="questionType"
               value={model.type}
-              onChange={(ev) =>
-                setModel({ ...model, type: ev.target.value })}
+              onChange={onTypeChange}
               className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
             >
               {questionTypes.map((type) => (
@@ -135,7 +170,63 @@ const QuestionEditor = ({
           ></textarea>
         </div>
         {/*Description*/}
+
+        <div>
+            {shouldHaveOptions() && (<div>
+                <h4 className='text-sm font-semibold mb-1 flex justify-between items-center'>Options
+                    <button type='button' className='flex items-center text-xs py-1 px-2 rounded-sm text-white bg-gray-600 hover:bg-gray-700' onClick={addOption}>Add</button>
+                </h4>
+                { model.data.options.length === 0 && (<div className='text-xs text-gray-600 text-center py-3'>
+                    Don't have any options
+                </div>)}
+                {model.data.options.length > 0 && (
+                <div>
+                  {model.data.options.map((op, ind) => (
+                    <div key={op.uuid} className="flex items-center mb-1">
+                      <span className="w-6 text-sm">{ind + 1}.</span>
+                      <input
+                        type="text"
+                        value={op.text}
+                        onInput={(ev) => {
+                          op.text = ev.target.value;
+                          setModel({ ...model });
+                        }}
+                        className="w-full
+                      rounded-sm
+                      py-1
+                      px-2
+                      text-xs
+                      border border-gray-300
+                      focus:border-indigo-500"
+                      />
+                      <button
+                        onClick={ev => deleteOption(op)}
+                        type="button"
+                        className="            h-6
+                        w-6
+                        rounded-full
+                        flex
+                        items-center
+                        justify-center
+                        border border-transparent
+                        transition-colors
+                        hover:border-red-100"
+                      >
+                        <TrashIcon className="w-3 h-3 text-red-500" />
+                      </button>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+            )}
+        </div>
+        {model.type === 'select' && <div>
+        </div>
+        }
+
       </div>
+      <hr />
     </>
   )
 }
