77b7c7788f30914facf3cdd44e1be3a19840fd8a
darienvr <118293353+darienvr@users.noreply.github.com>
2024-03-22 18:27:05 -0500

    add dashboard backend

--- app/Models/Survey.php
+++ app/Models/Survey.php
@@ -28,5 +28,10 @@ class Survey extends Model
     {
         return $this->hasMany(SurveyQuestion::class);
     }
+
+    public function answers()
+    {
+        return $this->hasMany(SurveyAnswer::class);
+    }
 }
 
